9a92700dcbf031c6cba18d64bc8fdf00e7d6c761
Dylon <dylon.tjanaka@gmail.com>
2020-06-26 07:06:52 +0000

    Add initial tictactoe logic.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -63,9 +63,50 @@ function changeProfilePic() {
   document.getElementById('pfp').src = '/images/' + img;
 }
 
+let xando = [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]];
+
 /**
- * Process tic-tac-toe clicks.
+ * Process tic-tac-toe game.
  */
 function ticTacToe(cell) {
+    const playerTurn = document.getElementById("player-turn").className;
+    const whichCell = document.getElementById(cell);
+    if (playerTurn === "0") {
+        whichCell.innerText = "❌";
+    }
+    else {
+        whichCell.innerText = "⭕";
+    }
+    whichCell.disabled = true;
     
+    if (cell < 4) { xando[+playerTurn][0]++; }
+    else if (cell < 7) { xando[+playerTurn][1]++; }
+    else { xando[+playerTurn][2]++; }
+
+    if (+playerTurn === 1 || +playerTurn === 4 || +playerTurn === 7) { xando[+playerTurn][3]++; }
+    else if (+playerTurn === 2 || +playerTurn === 5 || +playerTurn === 8) { xando[+playerTurn][4]++; }
+    else { xando[+playerTurn][5]++; }
+
+    if (+playerTurn === 1 || +playerTurn === 5 || +playerTurn === 9) { xando[+playerTurn][6]++; }
+    else if (+playerTurn === 3 || +playerTurn === 5 || +playerTurn === 7) { xando[+playerTurn][7]++; }
+
+    let winner;
+
+    for (let i = 0; i < 2; i++) {
+        for (let j = 0; j < 8; j++) {
+            if (xando[i][j] >= 3) {
+                winner = i;
+            }
+        }
+    }
+
+    if (winner !== undefined) {
+        for (let i = 1; i < 10; i++) {
+            document.getElementById(i).disabled = true;
+        }
+        document.getElementById("player-turn").innerText = winner ? "O" : "X" + " wins!";
+    }
+
+    document.getElementById("player-turn").className = +playerTurn ? "0" : "1";
+    document.getElementById("player-turn").innerText = "Player " + +playerTurn ? "0" : "1" + ", make your move.";
 }
