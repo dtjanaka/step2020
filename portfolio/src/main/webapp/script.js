2639faaaddf44c62a06e5c70e4e1ce12c71c7acb
Dylon <dylon.tjanaka@gmail.com>
2020-06-26 07:38:39 +0000

    Fix formatting and delete old elements.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -63,51 +63,66 @@ function changeProfilePic() {
   document.getElementById('pfp').src = '/images/' + img;
 }
 
-let xando = [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]];
+let xando = [
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+];
 
 /**
  * Process tic-tac-toe game.
  */
 function ticTacToe(cell) {
-    const playerTurn = document.getElementById("player-turn").className;
-    const whichCell = document.getElementById(cell);
-    if (playerTurn === "0") {
-        whichCell.innerText = "❌";
+  const playerTurn = document.getElementById('player-turn').className;
+  const whichCell = document.getElementById(cell);
+  if (playerTurn === '0') {
+    whichCell.innerText = '❌';
+  } else {
+    whichCell.innerText = '⭕';
+  }
+  whichCell.disabled = true;
+
+  if (cell < 4) {
+    xando[+playerTurn][0]++;
+  } else if (cell < 7) {
+    xando[+playerTurn][1]++;
+  } else {
+    xando[+playerTurn][2]++;
+  }
+
+  if (cell === 1 || cell === 4 || cell === 7) {
+    xando[+playerTurn][3]++;
+  } else if (cell === 2 || cell === 5 || cell === 8) {
+    xando[+playerTurn][4]++;
+  } else {
+    xando[+playerTurn][5]++;
+  }
+
+  if (cell === 1 || cell === 5 || cell === 9) {
+    xando[+playerTurn][6]++;
+  } else if (cell === 3 || cell === 5 || cell === 7) {
+    xando[+playerTurn][7]++;
+  }
+
+  let winner;
+
+  for (let i = 0; i < 2; i++) {
+    for (let j = 0; j < 8; j++) {
+      if (xando[i][j] >= 3) {
+        winner = i;
+      }
     }
-    else {
-        whichCell.innerText = "⭕";
+  }
+
+  if (winner !== undefined) {
+    for (let i = 1; i < 10; i++) {
+      document.getElementById(i).disabled = true;
     }
-    whichCell.disabled = true;
-    
-    if (cell < 4) { xando[+playerTurn][0]++; }
-    else if (cell < 7) { xando[+playerTurn][1]++; }
-    else { xando[+playerTurn][2]++; }
+    document.getElementById('player-turn').innerText =
+      'Player ' + (winner ? 'O' : 'X') + ' wins!';
+    return;
+  }
 
-    if (cell === 1 || cell === 4 || cell === 7) { xando[+playerTurn][3]++; }
-    else if (cell === 2 || cell === 5 || cell === 8) { xando[+playerTurn][4]++; }
-    else { xando[+playerTurn][5]++; }
-
-    if (cell === 1 || cell === 5 || cell === 9) { xando[+playerTurn][6]++; }
-    else if (cell === 3 || cell === 5 || cell === 7) { xando[+playerTurn][7]++; }
-
-    let winner;
-
-    for (let i = 0; i < 2; i++) {
-        for (let j = 0; j < 8; j++) {
-            if (xando[i][j] >= 3) {
-                winner = i;
-            }
-        }
-    }
-
-    if (winner !== undefined) {
-        for (let i = 1; i < 10; i++) {
-            document.getElementById(i).disabled = true;
-        }
-        document.getElementById("player-turn").innerText = "Player" + (winner ? "O" : "X") + " wins!";
-        return;
-    }
-
-    document.getElementById("player-turn").className = +playerTurn ? "0" : "1";
-    document.getElementById("player-turn").innerText = "Player " + (+playerTurn ? "X" : "O") + ", make your move.";
+  document.getElementById('player-turn').className = +playerTurn ? '0' : '1';
+  document.getElementById('player-turn').innerText =
+    'Player ' + (+playerTurn ? 'X' : 'O') + ', make your move.';
 }
