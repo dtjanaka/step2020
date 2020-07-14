0699eb38347b3074ea14037fdab8b70d85b7f190
Dylon <dylon.tjanaka@gmail.com>
2020-07-14 00:06:37 +0000

    Complete Greeter unit test practice exercises and move non-servlet Java files out of servlet directory.

--- walkthroughs/week-5-tdd/intro/src/main/java/com/google/sps/Greeter.java
+++ walkthroughs/week-5-tdd/intro/src/main/java/com/google/sps/Greeter.java
@@ -18,10 +18,34 @@ package com.google.sps;
  * Utility class for creating greeting messages.
  */
 public class Greeter {
+
+  /**
+   * Return the input with leading whitespace removed.
+   */
+  private StringBuilder removeSpaceFromFront(StringBuilder sb) {
+    while (sb.length() > 0 && sb.charAt(0) == ' ') {
+        sb.deleteCharAt(0);
+    }
+    return sb;
+  }
+
   /**
    * Returns a greeting for the given name.
    */
   public String greet(String name) {
-    return "Hello " + name;
+    StringBuilder sb = new StringBuilder();
+    sb.append(name);
+    sb = removeSpaceFromFront( removeSpaceFromFront(sb).reverse() ).reverse();
+
+    // Remove characters that are neither letters nor spaces.
+    for (int i = 0; i < sb.length(); i++) {
+        char c = sb.charAt(i);
+        if (!Character.isLetter(c) && c != ' ') {
+            sb.deleteCharAt(i);
+            i--;
+        }
+    }
+
+    return "Hello " + sb.toString();
   }
 }
