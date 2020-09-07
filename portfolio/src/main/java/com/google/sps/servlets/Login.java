d413c4cb6a2b817bae8af817562eedf478866635
Dylon Tjanaka <Dylon.tjanaka@gmail.com>
2020-09-06 21:35:25 -0700

    Update Login.java

--- portfolio/src/main/java/com/google/sps/servlets/Login.java
+++ portfolio/src/main/java/com/google/sps/servlets/Login.java
@@ -31,18 +31,19 @@ public class Login extends HttpServlet {
       throws IOException {
     response.setContentType("application/json");
 
-    String userInfo = new String();
+    String page = request.getParameter("page");
 
     UserService userService = UserServiceFactory.getUserService();
     if (userService.isUserLoggedIn()) {
-      String logoutUrl = userService.createLogoutURL("/#/logout");
+      String logoutUrl = userService.createLogoutURL("/" + page + ".html");
       Gson gson = new GsonBuilder().setPrettyPrinting().create();
-      userInfo = gson.toJson(new UserInfo(true, logoutUrl));
+      String userInfo = gson.toJson(new UserInfo(true, logoutUrl));
+      response.getWriter().println(userInfo);
     } else {
-      String loginUrl = userService.createLoginURL("/");
+      String loginUrl = userService.createLoginURL("/" + page + ".html");
       Gson gson = new GsonBuilder().setPrettyPrinting().create();
-      userInfo = gson.toJson(new UserInfo(false, loginUrl));
+      String userInfo = gson.toJson(new UserInfo(false, loginUrl));
+      response.getWriter().println(userInfo);
     }
-    response.getWriter().println(userInfo);
   }
 }
